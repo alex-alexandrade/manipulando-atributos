d71cac6cd2674ee495436b59e5ab03fe0edc306a
alex-alexandrade <alexandrade1516@hotmail.com>
2022-04-27 09:31:36 -0300

    first commit

---
+++ calculadora.js
@@ -0,0 +1,5 @@
+function somar(){
+    let n1 = document.getElementById('n1').value;
+    let n2 = document.getElementById('n2').value;
+    document.getElementById('#resultado').innerText = n1 + n2;
+}
